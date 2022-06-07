e235047ea2ce5c42475b530e47d0b936ad1c54e9
YoussefMohamedSalah <97204191+YoussefMohamedSalah@users.noreply.github.com>
2022-06-07 23:54:42 +0200

    first commit

---
+++ amazone_fe/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/"> Amazone</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
